b2a42f0600d9f5fcea09f5265967a1b8b4f20c2e
Angelica Moberg <angelica.moberg@hotmail.com>
2021-12-07 11:07:56 +0100

    Raderade onödig info på php-sidorna.
    Påverkade output

--- php/deleteHoroscope.php
+++
@@ -1,7 +0,0 @@
-<!-- Sidan ska bara gå att begära via DELETE, den ska ta bort det sparade horoskopet i 
-$_SESSION och echo:a true. Om inget finns sparat ska den echo:a false. -->
-
-<?php 
-        session_start();
-
-        ?>
