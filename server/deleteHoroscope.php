921865d2a590fe4df769e791d8fd93ef1ce663f4
Angelica Moberg <angelica.moberg@hotmail.com>
2021-12-09 11:41:13 +0100

    PHP

--- server/deleteHoroscope.php
+++ server/deleteHoroscope.php
@@ -1,9 +1,7 @@
-
 <?php 
 
-session_start();
-
 try {
+        session_start();
 
         if ($_SERVER['REQUEST_METHOD']) {
 
@@ -13,16 +11,23 @@ try {
                                 session_destroy();
 
                                 echo json_encode(true);
+                                exit;
                         } else {
                                 echo json_encode(false);
+                                exit;
                         }
+
+                } else {
+                        throw new Exception("Endpoint not valid", 405);
                 }
 
         } else {
-                echo json_encode(false); 
+                throw new Exception("http-request not sent", 404);
         }
 
 } catch(Exception $err) {
-        error_log($err);
+        http_response_code($err -> getCode());
+        echo json_encode($err -> getMessage()); 
+        exit;
 }
 ?>
--- server/updateHoroscope.php
+++ server/updateHoroscope.php
@@ -1,34 +1,46 @@
-
 <?php 
-session_start();
 
 try {
+        session_start();
 
         if ($_SERVER['REQUEST_METHOD']) {
 
                 if($_SERVER["REQUEST_METHOD"] == "POST") {
 
                         if(isset($_SESSION["horoscope"])) {
-                                $inputDate =  $_POST["inputDate"]; 
 
-                                require_once("./listHoroscope.php");
-                
-                                $horoscope = getOutput($_POST["inputDate"], $getList);
-                                $_SESSION["horoscope"] = $horoscope;
-                
-                                echo json_encode(true);
+                                if(isset($_POST["inputDate"])) {
 
+                                        $inputDate =  $_POST["inputDate"]; 
+
+                                        require_once("./listHoroscope.php");
+                        
+                                        $horoscope = getOutput($_POST["inputDate"], $getHoroscope);
+                                        $_SESSION["horoscope"] = $horoscope;
+                        
+                                        echo json_encode(true);
+                                        exit;
+
+                                } else {
+                                        throw new Exception("Check input", 400);
+                                }
+                                       
                         } else{
                                 echo json_encode(false);
+                                exit;
                         } 
 
+                } else {
+                        throw new Exception("Endpoint not valid", 405);
                 }
 
         } else {
-                echo json_encode(false); 
+                throw new Exception("http-request not sent", 404);
         }
 
 } catch(Exception $err) {
-        error_log($err);
+        http_response_code($err -> getCode());
+        echo json_encode($err -> getMessage()); 
+        exit;
 }
 ?>
